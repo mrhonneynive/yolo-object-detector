88b1430d506e3ff58c84538bfbfad3e32966855e
mrhonneynive <38505964+mrhonneynive@users.noreply.github.com>
2025-05-27 18:11:52 +0300

    Support both /detect and /detect/{label} endpoints

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.post("/detect/")
 @app.post("/detect/{label}")
-async def detect_image(label: str, file: Annotated[UploadFile, File()]):
+async def detect_image(file: Annotated[UploadFile, File()],label: str | None = None):
   return {"message": "it works"}
