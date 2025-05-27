3a9d118d851503e7bf84cc189dd8abbb58cf05d0
mrhonneynive <38505964+mrhonneynive@users.noreply.github.com>
2025-05-27 18:00:55 +0300

    Implement /detect/{label} file upload

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/detect/{label}")
+async def detect_image(label: str, file: Annotated[UploadFile, File()]):
+  return {"message": "it works"}
